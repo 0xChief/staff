ade306795d01095d949336ad1ad6c5e4e2988bea
0x <s@x.ai>
2024-01-24 20:31:56 +0700

    feat: Add shpunks

--- src/main/kotlin/io/ordex/ethescription/collections/ResetCollectionMeta.kt
+++ src/main/kotlin/io/ordex/ethescription/collections/ResetCollectionMeta.kt
@@ -9,7 +9,7 @@ import org.springframework.web.client.RestTemplate
 fun main(args: Array<String>) = runBlocking<Unit> {
     val repository = CollectionRepository()
 
-    listOf("unpunks")
+    listOf("mfpurrfect")
         .map { repository.get(it)!! }
         .map { it.collectionItems }
         .flatten()
