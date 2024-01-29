6dcfa54e0b7722caed171286e5dee70fc3fe4f5c
0x <s@x.ai>
2024-01-29 12:14:12 +0700

    Update bulls

--- src/main/kotlin/io/ordex/ethescription/collections/ResetCollectionMeta.kt
+++ src/main/kotlin/io/ordex/ethescription/collections/ResetCollectionMeta.kt
@@ -9,7 +9,7 @@ import org.springframework.web.client.RestTemplate
 fun main(args: Array<String>) = runBlocking<Unit> {
     val repository = CollectionRepository()
 
-    listOf("nodeplayers")
+    listOf("thedarwins")
         .map { repository.get(it)!! }
         .map { it.collectionItems }
         .flatten()
