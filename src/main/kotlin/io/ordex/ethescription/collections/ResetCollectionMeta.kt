8e2c955e8eecdc0f12bd9449fea6d81f0a5d135e
0x <s@x.ai>
2024-01-31 08:33:18 +0700

    feat: Fix plushscriptions

--- src/main/kotlin/io/ordex/ethescription/collections/ResetCollectionMeta.kt
+++ src/main/kotlin/io/ordex/ethescription/collections/ResetCollectionMeta.kt
@@ -11,7 +11,7 @@ fun main(args: Array<String>) = runBlocking<Unit> {
     val repository = CollectionRepository()
     val rest = WebClientProvider.initTransport()
 
-    listOf("thedarwins")
+    listOf("plushscriptions")
         .map { repository.get(it)!! }
         .map { it.collectionItems }
         .flatten()
